5e6d34e8f13df0099d310cfe413f57ebabf347d7
mj-meo-dmt <mjmeodmt@gmail.com>
2015-09-09 15:45:03 +0200

    Added light and basic texture, fixed heightfield

--- src/terrain.py
+++ src/terrain.py
@@ -5,7 +5,7 @@
 import sys
 
 ### PANDA Imports ###
-from panda3d.core import GeoMipTerrain
+from panda3d.core import GeoMipTerrain, DirectionalLight, VBase4
 
 
 ########################################################################
@@ -13,8 +13,19 @@ from panda3d.core import GeoMipTerrain
 class Terrain():
 
     def __init__(self, _heightField):
+
+    	texture = loader.loadTexture("ground.png")
         
         self.terrain = GeoMipTerrain("BasicTerrain")
         self.terrain.setHeightfield(_heightField)
+        self.terrain.getRoot().setSz(25)
+        self.terrain.getRoot().setTexture(texture)
         self.terrain.getRoot().reparentTo(render)
         self.terrain.generate()
+
+        # Some Test light
+        dlight = DirectionalLight("dlight")
+        dlight.setColor(VBase4(0.8, 0.8, 0.5, 1))
+        dlnp = render.attachNewNode(dlight)
+        dlnp.setHpr(0, -60, 0)
+        render.setLight(dlnp)
