7120f1e35e8d79f25a9ab17aa78cf14689d9ec37
fireclawthefox <fireclawthefox@gmail.com>
2015-09-11 10:53:49 +0200

    run player from main script

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import sys
 ### PANDA Imports ###
 from direct.showbase.ShowBase import ShowBase
 from terrain import Terrain
-
+from player import Player
 
 ########################################################################
 
@@ -15,16 +15,21 @@ class Main(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
+        # accept the esc button to close the application
+        self.accept("escape", sys.exit)
+
+        # disable pandas default mouse-camera controls so we can handle the cam
+        # movements by ourself
+        self.disableMouse()
 
         # Need First person character
         # - Basic pickaxe animation
         # Need Basic gui drag and drop
         # Need mining nodes
         t = Terrain("map.png")
-        
 
-
-
+        p = Player()
+        p.run()
 
 
 game = Main()
